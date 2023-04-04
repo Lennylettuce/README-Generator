027b4ca48d194f9b9f4c3158b0f1024129ed458e
Aubrey Gunn <agunn369@gmail.com>
2023-04-03 21:48:44 -0500

    added questions for user input, added inquirer, and writefile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,142 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require('fs');
+const inquirer = require('inquirer');
+// create a const for where the generated markdown page will be in your files
+const createPage = require('./utils/generateMarkdown.js');
+// TODO: Create an array of questions for user input inside the questions function
+const questions = () => {
+    return inquirer.prompt([
+        
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false; 
+                }
+            }
+        }, 
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose your project license.',
+            choices: ['MIT', 'GNU'],
+            default: ["MIT"],
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'How can users install your application?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter steps required to install your project!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How do you use this application?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a usage description!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test', 
+            message: 'What command should be run to run tests?',
+            default: 'npm test'
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'How can fellow coders contribute to your Repo?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username!');
+                    return false; 
+                }
+            } 
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address!');
+                    return false; 
+                }
+            }
+    
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+}; 
 
-// TODO: Create a function to initialize app
-function init() {}
+// TODO: Call function to initialize app
+quesitons()
 
-// Function call to initialize app
-init();
+.then(response => {
+    return createPage(response);
+})
+.then(data = > {
+    return writeFile(data);
+})
+.catch(err => {
+    console.log(err)
+})
+//get user response and put it in createPage, display data in writefile on page and catch errors
